f6010d1cc471864885c4e9e92af85b2f3df6b354
mn <manan8@qq.com>
2023-03-06 11:31:35 +0800

    first test

---
+++ mlpricexcx/src/main/java/com/ml/mlpricexcx/MlpricexcxApplication.java
@@ -0,0 +1,13 @@
+package com.ml.mlpricexcx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlpricexcxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlpricexcxApplication.class, args);
+    }
+
+}
